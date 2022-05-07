12695fa7251f7a9bde03e10bd0d4c0f1be7ee24e
IgorKilipenko <igorkili@mail.ru>
2022-05-07 07:10:22 +0700

    include libserialport

--- lib/flutter_libserialport.dart
+++ lib/flutter_libserialport.dart
@@ -1,7 +1,12 @@
 
 import 'flutter_libserialport_platform_interface.dart';
 
-export 'package:libserialport/libserialport.dart';
+export 'package:flutter_libserialport/src/libserialport.dart';
+//export 'src/config.dart' show SerialPortConfig;
+//export 'src/enums.dart';
+//export 'src/error.dart';
+//export 'src/port.dart' show SerialPort;
+//export 'src/reader.dart' show SerialPortReader;
 
 class FlutterLibserialport {
   Future<String?> getPlatformVersion() {
