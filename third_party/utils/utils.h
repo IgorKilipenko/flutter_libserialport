dfd1cb210fc539a0a34e3a0ac2af67f657ac1b64
IgorKilipenko <igorkili@mail.ru>
2022-05-16 02:08:50 +0700

    add flutter trace debug

--- third_party/utils/utils.h
+++ third_party/utils/utils.h
@@ -1,7 +1,10 @@
 #ifndef _SP_UTILS_H
 #define _SP_UTILS_H
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <locale.h>
+#include "libserialport_internal.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -13,9 +16,12 @@ extern "C" {
 #define EXPORT
 #endif
 
-
+void (*utils_debug_handler)(const char *format, size_t length);
 //EXPORT _locale_t createLocale();
 EXPORT char* utils_geCurrenttLocaleName();
+EXPORT void utils_printf(const char *format, ...) ;
+EXPORT void utils_set_debug_handler(void (const char *str, size_t length));
+EXPORT void utils_init_debug();
 
 #ifdef __cplusplus
 }
